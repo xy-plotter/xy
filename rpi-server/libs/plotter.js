30ea463dd24be1ad01537b3569f1cb41d12de4ed
RNO <arnaud.juracek@gmail.com>
2016-09-03 00:40:59 +0200

    server: many commands !

--- rpi-server/libs/plotter.js
+++ rpi-server/libs/plotter.js
@@ -3,106 +3,210 @@ const sh = require('kool-shell');
 
 let data = null;
 let buffer = [];
-
-const WIDTH = 380;
-const HEIGHT = 310;
+const WIDTH = 310;
+const HEIGHT = 380;
 
 const Plotter = {
   data, buffer,
   WIDTH, HEIGHT,
 
-  connect(_port, _baudRate) {
-    port = new serial(_port, {
-      baudRate: _baudRate,
+  // -------------------------------------------------------------------------
+  // SERIAL
+
+  connect(address, baudRate, verbose = false) {
+    this.verbose = verbose;
+    this.port = new serial(address, {
+      baudRate: baudRate,
       parser: serial.parsers.readline('\n'),
     });
 
     return new Promise((resolve, reject) => {
-      port.on('open', function() {
-        sh.success(`connected to ${_port}`);
+      this.port.on('open', function() {
+        sh.info(`connecting to ${address}...`);
         sh.info(`waiting for handshake...`);
       });
 
-      port.on('data', (data) => {
+      this.port.on('data', (data) => {
         resolve(data);
+        // sh.info(data);
         this.processBuffer();
       });
 
-      port.on('error', function(err) {
+      this.port.on('error', (err) => {
         reject(new Error(err, 'plotter.js'));
       });
+    }).then(() => {
+      sh.success('connected.');
+      this.connected = true;
+    });
+  },
+
+  disconnect() {
+    return new Promise((resolve, reject) => {
+      if (this.port) {
+        this.port.close(() => {
+          resolve('disconnected.');
+        });
+      } else reject(new Error('port is alread disconnected.'));
+    }).then((res) => {
+      sh.success(res);
+      this.connected = false;
+    }).catch((err) => {
+      sh.error(err);
     });
   },
 
   // -------------------------------------------------------------------------
+  // COMMUNICATION
 
-  sendMessage(message, verbose = false) {
-    if (port) {
-      message = message + '\n';
-      port.write(message, function(err) {
+  sendMessage(message) {
+    if (this.port) {
+      this.port.write(`${message}\n`, (err) => {
         if (err) sh.error(new Error(err, 'plotter.js'));
         else {
-          if (verbose) sh.info(message);
+          if (this.verbose) sh.info(message);
         }
       });
     } else sh.error(new Error('port isn\'t openned.', 'plotter.js'));
   },
 
-  addToBuffer(message, verbose = false) {
-    if (verbose) sh.info(`"${message}" buffered.`);
-    this.buffer.push(message);
-  },
+  addToBuffer(message) { this.buffer.push(message); },
 
   processBuffer() {
     if (this.buffer.length > 0) {
       let message = this.buffer.shift();
-      this.sendMessage(message);
+      if (message === -1 && this.onEndCallback) this.onEndCallback();
+      else this.sendMessage(message);
     }
   },
 
   // -------------------------------------------------------------------------
+  // SETUP COMMANDS
+
+  setSpeed(speed) {
+    this.addToBuffer(`S ${speed}`);
+    return this;
+  },
+
+  drawBoundaries() {
+    this
+      .home()
+      .pen_down()
+      // FIXME : the plotter can't go from 0 to HEIGHT in one move (???)
+      .move(0, this.HEIGHT / 2)
+      .move(0, this.HEIGHT)
+      .move(this.WIDTH, this.HEIGHT)
+      .move(this.WIDTH, this.HEIGHT / 2)
+      .move(this.WIDTH, 0)
+      .move(0, 0);
+    return this;
+  },
+
+  // -------------------------------------------------------------------------
+  // UTILS COMMANDS
 
   getPosition() {
     this.addToBuffer('P');
     return this;
   },
 
-  pen(position, verbose = false) {
-    this.addToBuffer(`M1 ${position}`, verbose);
+  home() {
+    this.pen_up();
+    this.addToBuffer('G28');
     return this;
   },
 
-  move(x, y, verbose = false) {
-    if (x < 0 || x > WIDTH || y < 0 && y > HEIGHT) {
-      sh.warning('warning : position will be outside plotter\'s boundaries.')
+  end(callback) {
+    this.addToBuffer(-1);
+    this.onEndCallback = callback;
+    return this;
+  },
+
+  move(x, y) {
+    if (x < 0 || x > this.WIDTH || y < 0 || y > this.HEIGHT) {
+      sh.warning(`G1 X${x} Y${y} : position will be outside plotter's boundaries.`);
     }
-    this.addToBuffer(`G1 X${x} Y${y}`, verbose);
-    return this;
-  },
-
-  home(verbose = false) {
-    this.addToBuffer('G28', verbose);
+    this.addToBuffer(`G1 X${x} Y${y}`);
     return this;
   },
 
   // -------------------------------------------------------------------------
+  // PEN COMMANDS
 
-  pen_up(verbose = false) {
-    this.pen(0, verbose);
+  pen(position) {
+    this.addToBuffer(`M1 ${position}`);
     return this;
   },
 
-  pen_down(verbose = false) {
-    this.pen(90, verbose);
+  pen_up(force_motion = false) {
+    if (!this.up ||force_motion) {
+      this.up = true;
+      this.pen(0);
+    }
     return this;
   },
 
-  draw(x, y, verbose = false) {
-    this.pen_down(verbose);
-    this.move(x, y, verbose);
-    this.pen_up(verbose);
+  pen_down(force_motion = false) {
+    if (this.up || force_motion) {
+      this.up = false;
+      this.pen(90);
+    }
     return this;
   },
+
+  // -------------------------------------------------------------------------
+  // 2D PRIMITIVES
+
+  point(x, y) {
+    this.move(x, y);
+    // TODO : find a better temporisation method
+    for (let i = 0; i < 2; i++) this.pen_down(true);
+    this.pen_up();
+    return this;
+  },
+
+  polygon(points) {
+    this
+      .pen_up()
+      .move(points[0][0], points[0][1])
+      .pen_down();
+
+    for (let i = 1; i < points.length; i++) {
+      let point = points[i];
+      this.move(point[0], point[1]);
+    }
+    return this;
+  },
+
+  line(x1, y1, x2, y2) {
+    return this.polygon([[x1, y1], [x2, y2]]);
+  },
+
+  ellipse(cx, cy, w, h, sides = 100) {
+    let points = [];
+    for (let theta = 0; theta <= Math.PI * 2; theta += (Math.PI / sides)) {
+      let x = cx + Math.sin(theta) * w;
+      let y = cy + Math.cos(theta) * h;
+      points.push([x, y]);
+    }
+    return this.polygon(points);
+  },
+
+  circle(cx, cy, r, sides = 100) {
+    return this.ellipse(cx, cy, r, r, sides);
+  },
+
+  triangle(x1, y1, x2, y2, x3, y3) {
+    return this.polygon([[x1, y1], [x2, y2], [x3, y3], [x1, y1]]);
+  },
+
+  quad(x1, y1, x2, y2, x3, y3, x4, y4) {
+    return this.polygon([[x1, y1], [x2, y2], [x3, y3], [x4, y4], [x1, y1]]);
+  },
+
+  rect(x, y, w, h) {
+    return this.polygon([[x, y], [x + w, y], [x + w, y + h], [x, y + h], [x, y]]);
+  }
 
 
 }
